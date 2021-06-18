b9a9f89f82c5d332bdba68132b00b3941a3a4387
Oliver Joseph Ash <oliverjash@gmail.com>
2021-06-18 21:03:54 +0100

    WIP

--- server-client.js
+++ server-client.js
@@ -4,12 +4,4 @@ export const server = getRemote({ standaloneServerUrl: 'http://localhost:1773' }
 
 console.log('registering before hook')
 
-before(async () => {
-  // Starts the server on a dynamic port. The port number that can later be
-  // retrieved as server.url
-  await server.start(8080)
-})
-
-after(async () => {
-  await server.stop()
-})
+before(() => server.useConfig({ port: 8080, mockRoot: 'http://localhost:8080' }));
--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
-import {getStandalone, getLocal} from 'mockttp'
+import {getStandalone, getLocal, getRemote} from 'mockttp'
 
 const mockServerManager = getStandalone({ debug: true })
 const mockServer = mockServerManager.start(1773)
 
-// Mockttp does not have a health check endpoint, so I create a separate
-// mock server just for that.
-const local = getLocal()
-local.start(9999)
-local.get("/").thenReply(200, 'Mock server is up')
+getRemote({ standaloneServerUrl: 'http://localhost:1773' }).start(8080).then(() => {
+  // Mockttp does not have a health check endpoint, so I create a separate
+  // mock server just for that.
+  const local = getLocal()
+  local.start(9999)
+  local.get("/").thenReply(200, 'Mock server is up')
+})
 
 mockServer.then(() => {
   console.log('Mock server manager is started')
