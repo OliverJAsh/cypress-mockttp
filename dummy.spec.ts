18d6537c0df6cdb2f8180f43957972eecb726366
Oliver Joseph Ash <oliverjash@gmail.com>
2021-06-18 19:37:06 +0100

    Port

--- dummy.spec.ts
+++ dummy.spec.ts
@@ -8,7 +8,7 @@ const mockedResponse = 'this is a mocked response'
 before(async () => {
   // Starts the server on a dynamic port. The port number that can later be
   // retrieved as server.url
-  await server.start()
+  await server.start(8080)
   await server.get("/mocked-path").thenReply(200, mockedResponse)
 })
 
@@ -20,7 +20,7 @@ describe('Mockttp serves mocked responses', () => {
   it('to cy.request', () => {
     // We make the request from Cypress' Node.js process, and it receives
     // the mocked response we pre-programmed just before
-    const url = server.url + '/mocked-path'
+    const url = 'http://localhost:8080/mocked-path'
     cy.request(url).its('body').should('equal', mockedResponse)
   })
 })
